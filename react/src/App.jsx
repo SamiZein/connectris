32970c674e323d2c79d2426f9c31b30b6565660d
SamiZeinAI <zeins20@mycu.concord.edu>
2024-07-07 11:52:10 -0400

    Updated state of tiles to be handle at the board level instead of at the column level

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,19 +1,29 @@
 import { useState } from 'react'
 import Board from './assets/Connect4Board.png'
 import Column from './Column'
+import Tile from './Tile'
+
 
 function App() {
+  const [columns, setColumns] = useState([[],[],[],[],[],[], []])
+  const addTile = (columnIndex) => () => {
+    
+    if (columns[columnIndex].length > 6) return
+    console.log(columnIndex)
+    console.log("in addTile function")
+    const newColumns = [...columns]
+    newColumns[columnIndex].push(<Tile className="bg-red-500" />)
+    setColumns(newColumns)
+  }
 
   return (
-    <div>
+    <div onClick={addTile}>
       <div className="h-[29.7rem] ml-4 absolute flex space-x-[1.2rem]">
-        <Column />
-        <Column />
-        <Column />
-        <Column />
-        <Column />
-        <Column />
-        <Column />
+        {columns.map((column, index) =>  
+          <div className="h-auto flex justify-end" key={index} onClick={addTile(index)}>
+            <Column tiles={column} />
+          </div>)
+        }
       </div>
       
       <img src={Board} alt="board" />
--- react/src/Column.jsx
+++ react/src/Column.jsx
@@ -1,21 +1,10 @@
-import { useState } from 'react'
-import Tile from './Tile.jsx'
-export default function Column() {
-    const [tiles, setTiles] = useState([])
-    const addTile = () => {
-        if (tiles.length > 6) return
-        setTiles([...tiles, <Tile className="bg-red-500" />])
-    }
 
+export default function Column({tiles}) {
     return (
-        <div className="flex flex-col justify-end h-auto w-[4.4rem] pt-[.29rem] space-y-[.6rem] z-10"
-            onClick={addTile}
-        >
-            <Tile className="bg-red-500" />
+        <div className="flex flex-col justify-end h-auto w-[4.4rem] pt-[.29rem] space-y-[.6rem] z-10">
             {tiles &&
                 tiles.map((tile, index) => (
-                    <div key={index}> {tile}</div>
-                   
+                    <div key={index}> {tile}</div>                 
             ))}
         </div>
     )
